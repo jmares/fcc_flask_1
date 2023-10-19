3ca5ac1315a4a36b74705a08a18f06c4aedffe85
jmares <67288966+jmares@users.noreply.github.com>
2023-10-19 12:39:11 +0200

    lesson 01 added a second page

--- market.py
+++ market.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '<h1>Hello, World!</h1><h2>Some Text</h2><p>Lorum ipsum ...</p>'
+
+@app.route('/about')
+def about_page():
+    return '<h1>Hello, World!</h1><h2>This Site</h2><p>Lorum ipsum ...</p><h2>The Owner</h2><p>Lorum ipsum ...</p>'
